7c87b455c9b6ff3417e0c0dd8890d7cb5b7beb3b
Nilton OS <nioliveira@equinix.com>
2020-01-21 14:21:41 -0300

    Update utils

--- src/utils.rs
+++ src/utils.rs
@@ -7,14 +7,11 @@ extern crate cmd_lib;
 use cmd_lib::run_cmd;
 use users::{get_current_uid, get_user_by_uid};
 
-pub fn get_distro() -> String {
-    get_cmd(String::from("ls -ila"));
-    let distro = String::from("cat /etc/*-release|tr [:upper:] [:lower:]|grep -Poi '(debian|ubuntu|red hat|centos|oracle|opensuse|sles)'|head -n1|tr -d ' '");
+pub fn get_distro() -> &'static str {
+    get_cmd("ls -ila");
+    let distro = "cat /etc/*-release|tr [:upper:] [:lower:]|grep -Poi '(debian|ubuntu|red hat|centos|oracle|opensuse|sles)'|head -n1|tr -d ' '";
     let os_name = run_sh(distro);
-
-    if os_name == "ubuntu" {
-        get_cmd(String::from("apt-get update"));
-    }
+    // if os_name == "ubuntu" || os_name == "debian" { get_cmd("apt-get update"); }
     return os_name;
 }
 
@@ -27,17 +24,27 @@ pub fn get_sudo() {
     }
 }
 
-fn get_cmd(cmd: String) {
+fn get_cmd(cmd: &str) {
     run_cmd!("{}", cmd);
 }
 
-fn run_sh(cmd: String) -> String {
+fn run_sh(cmd: &str) -> &str {
     let output = Command::new("sh")
         .arg("-c")
-        .arg(&cmd)
+        .arg(cmd)
         .output()
         .expect("Failed run_sh");
 
-    let run = String::from_utf8(output.stdout).unwrap();
-    return run.trim().to_string();
+    if output.status.success() {
+        let mut run = String::from_utf8(output.stdout).unwrap();
+        run = run.trim().to_string();
+        return Box::leak(run.into_boxed_str());
+    } else {
+        println!("status: {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        let mut run = String::from_utf8(output.stderr).unwrap();
+        run = run.trim().to_string();
+        return Box::leak(run.into_boxed_str());
+    }
 }
