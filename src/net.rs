fcc3aee50eef171d60005463c11b837833ce48f4
Nilton OS <nioliveira@equinix.com>
2020-01-22 12:57:14 -0300

    Add get ips

--- src/net.rs
+++ src/net.rs
@@ -3,6 +3,11 @@ use pnet::datalink;
 
 pub fn get_interfaces() {
     for iface in datalink::interfaces() {
-        println!("{:?}", iface.ips);
+        for ip in iface.ips.iter() {
+            if ip.is_ipv4() && iface.name != "lo" {
+                print!("Interface:{} Inet:{}; ", iface.name, ip);
+            }
+        }
     }
+    print!("\n");
 }
